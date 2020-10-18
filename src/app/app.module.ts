a29bc2a7df0b2040024f5f22804b8d98b48f110e
atliakos <atliakosg4@gmail.com>
2020-10-19 00:23:44 +0300

    adding files.

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+import { WrapperModule } from './wrapper/wrapper.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HelloWorldModule,
+    WrapperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
